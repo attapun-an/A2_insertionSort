707613eb22143a6591fc18ab7ed34ef844ecd55a
attapun-an <attapunanivat@gmail.com>
2017-09-03 23:16:23 +0700

    translating pseudocode
    This was the code from Friday. We were overcomplicating things instead
    of just following the pseudocode on the white board. It mostly works
    save for the first item not being shifting (leave that for later)

--- mainV2.py
+++ mainV2.py
@@ -1,15 +1,13 @@
 listOfNumbers = [23, 14, 96, 57, 2, 32]
 
 def insertion_sort(numList):
-
-    for i in range(1, len(numList)):
-        currItem = numList[i]
-        y = i
-        for y in range(y, 0, -1):
-            if currItem < numList[i]:
-                numList[y+1] = numList[y]
-            if currItem > numList[y]:
-                numList[y] = currItem
+    for i in range (1, len(numList)):
+        itemToBeInserted = numList[i]
+        currItem = i-1
+        while numList[currItem] > itemToBeInserted and currItem > 0:
+            numList[currItem+1] = numList[currItem]
+            currItem -= 1
+        numList[currItem+1] = itemToBeInserted
     return numList
 
 reSort = insertion_sort(listOfNumbers)
