70918ef6c29fa27a8d48667cec4a7c5737c0dcc0
attapun-an <attapunanivat@gmail.com>
2017-09-04 09:39:18 +0700

    abort mission (redo bubble sort)
    created bubbleSortV2 - will try to actually think this time.

---
+++ bubbleSortV2.py
@@ -0,0 +1,15 @@
+def bubble_sort(numList):
+    swap = True
+    while Swap:
+        swap = False
+        for i in range(len(numList)-1):
+            if numList[i] > numList[i+1]:
+                temp = numList[i]
+                numList[i] = numList[i+1]
+                
+
+    return numList
+
+listOfNumbers = [23, 14, 96, 57, 2, 32]
+meh = bubble_sort(listOfNumbers)
+print(meh)
